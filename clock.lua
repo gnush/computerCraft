27915fe02fd71a6717c183c8cd8e8aee4eeb7303
gnush <gnush@hushmail.com>
2012-05-13 12:25:25 +0200

    clock mainly finished

--- clock.lua
+++ clock.lua
@@ -1,6 +1,26 @@
 --[[
     Author: gnush
     This will set up an output depending on the daytime.
-    Morning: 0
-    Evening: 1
+    Noon: 0
+    Night: 1
 ]]--
+
+outSide = "bottom"
+
+function time()
+    local t = os.time()
+    
+    if ( t > 22.0 && t < 7.0) then
+        rs.setOutput(outSide, true)
+    else
+        rs.setOutput(outSide, false)
+    end
+end
+
+function loop()
+    time()
+    os.sleep(10)
+    loop()
+end
+
+loop()
